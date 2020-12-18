1215df6942ad4218066f6a8f55ef57aa07f1ddaa
rom <romain.signes@gmail.com>
2020-12-18 14:49:15 +0100

    login template driven form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData: { username: string, password: string } = {
+    username: '',
+    password: ''
+  }
+
+  handleSubmit() {
+    if (this.loginData.username == 'romain' && this.loginData.password == '1234') {
+      this.router.navigateByUrl('/catalog')
+    }
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
